7a808b999e43571c4863dc1ee7d6ec7f18821b3f
Peachex <klevolex@gmail.com>
2021-06-22 16:16:58 +0300

    fix bugs

--- repository/src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
+++ repository/src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
@@ -1,52 +1,55 @@
 package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.TagDao;
-import com.epam.esm.dao.constant.SqlTagQuery;
-import com.epam.esm.dao.mapper.TagMapper;
 import com.epam.esm.dto.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class TagDaoImpl implements TagDao<Tag> {
-    private final EntityManager em;
+    private final EntityManagerFactory factory;
 
     @Autowired
-    public TagDaoImpl(EntityManager em) {
-        this.em = em;
+    public TagDaoImpl(EntityManagerFactory factory) {
+        this.factory = factory;
     }
 
     @Override
     public boolean insert(Tag tag) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(tag);
+        em.getTransaction().commit();
         return true;
     }
 
     @Override
     public Optional<Tag> findById(long id) {
-        return Optional.ofNullable(em.find(Tag.class, id));
+        return Optional.ofNullable(factory.createEntityManager().find(Tag.class, id));
     }
 
     @Override
     public Optional<Tag> findByName(String name) {
+        EntityManager em = factory.createEntityManager();
         CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery<Tag> tagCriteria = criteriaBuilder.createQuery(Tag.class);
         Root<Tag> tagRoot = tagCriteria.from(Tag.class);
         tagCriteria.where(criteriaBuilder.equal(tagRoot.get("name"), name));
-        return Optional.ofNullable(em.createQuery(tagCriteria).getSingleResult());
+        return em.createQuery(tagCriteria).getResultStream().findAny();
     }
 
     @Override
     public List<Tag> findAll() {
+        EntityManager em = factory.createEntityManager();
         CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery<Tag> tagCriteria = criteriaBuilder.createQuery(Tag.class);
         Root<Tag> tagRoot = tagCriteria.from(Tag.class);
@@ -61,6 +64,11 @@ public class TagDaoImpl implements TagDao<Tag> {
 
     @Override
     public boolean delete(long id) {
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        Tag tag = findById(id).get();
+        em.remove(em.contains(tag)? tag : em.merge(tag));
+        em.getTransaction().commit();
         return true;
     }
 }
