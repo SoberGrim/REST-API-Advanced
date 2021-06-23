d48323152032aca2ebdadb2aecc5d942bd30f24e
Peachex <klevolex@gmail.com>
2021-06-23 12:46:21 +0300

    add error message localization

--- repository/src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
+++ repository/src/main/java/com/epam/esm/dao/impl/TagDaoImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
