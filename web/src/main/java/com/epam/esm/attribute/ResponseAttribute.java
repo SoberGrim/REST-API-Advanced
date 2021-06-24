18e45191798dca324c97e2106aff18739ad9058c
Peachex <klevolex@gmail.com>
2021-06-24 15:02:06 +0300

    delete javadoc

--- web/src/main/java/com/epam/esm/attribute/ResponseAttribute.java
+++ web/src/main/java/com/epam/esm/attribute/ResponseAttribute.java
@@ -5,6 +5,9 @@ public class ResponseAttribute {
     public static final String LOCALE_CHANGE_OPERATION = "operation.localeChanging";
     public static final String TAG_DELETE_OPERATION = "operation.tagDeletion";
     public static final String TAG_CREATE_OPERATION = "operation.tagCreation";
+
+    public static final String CERTIFICATE_CREATE_OPERATION = "operation.certificateCreation";
+
     public static final String CREATION_OPERATION = "operation.creation";
     public static final String DELETION_OPERATION = "operation.deletion";
     public static final String OTHER_OPERATION = "operation.other";
--- web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ web/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -1,6 +1,8 @@
 package com.epam.esm.controller;
 
+import com.epam.esm.attribute.ResponseAttribute;
 import com.epam.esm.dto.GiftCertificate;
+import com.epam.esm.response.OperationResponse;
 import com.epam.esm.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,44 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/**
- * The type Gift certificate controller.
- */
 @RestController
 @RequestMapping("/certificates")
 public class GiftCertificateController {
     private final GiftCertificateService<GiftCertificate> service;
 
-    /**
-     * Instantiates a new Gift certificate controller.
-     *
-     * @param service the service
-     */
     @Autowired
     public GiftCertificateController(GiftCertificateService<GiftCertificate> service) {
         this.service = service;
     }
 
-    /**
-     * Find all gift certificates list.
-     *
-     * @return the list
-     */
     @GetMapping("/all")
     public List<GiftCertificate> findAllGiftCertificates() {
         return service.findAll();
     }
 
-    /**
-     * Find certificates with tags list.
-     *
-     * @param tagName                the tag name
-     * @param certificateName        the certificate name
-     * @param certificateDescription the certificate description
-     * @param sortByName             the sort by name
-     * @param sortByDate             the sort by date
-     * @return the list
-     */
     @GetMapping
     public List<GiftCertificate> findCertificatesWithTags(@RequestParam(required = false) String tagName,
                                                           @RequestParam(required = false) String certificateName,
@@ -65,35 +44,19 @@ public class GiftCertificateController {
                 sortByDate);
     }
 
-    /**
-     * Create gift certificate response entity.
-     *
-     * @param giftCertificate the gift certificate
-     * @return the response entity
-     */
     @PostMapping("/new")
-    public ResponseEntity<String> createGiftCertificate(@RequestBody GiftCertificate giftCertificate) {
-        service.insert(giftCertificate);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Certificate created successfully");
+    public OperationResponse createGiftCertificate(@RequestBody GiftCertificate giftCertificate) {
+        OperationResponse response = new OperationResponse(OperationResponse.Operation.CREATION,
+                ResponseAttribute.CERTIFICATE_CREATE_OPERATION, String.valueOf(service.insert(giftCertificate)));
+
+        return response;
     }
 
-    /**
-     * Find certificate by id gift certificate.
-     *
-     * @param id the id
-     * @return the gift certificate
-     */
     @GetMapping("/{id}")
     public GiftCertificate findCertificateById(@PathVariable String id) {
         return service.findById(id);
     }
 
-    /**
-     * Delete gift certificate response entity.
-     *
-     * @param id the id
-     * @return the response entity
-     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteGiftCertificate(@PathVariable String id) {
         service.delete(id);
@@ -101,13 +64,6 @@ public class GiftCertificateController {
                 " (id = " + id + ")");
     }
 
-    /**
-     * Update gift certificate response entity.
-     *
-     * @param id              the id
-     * @param giftCertificate the gift certificate
-     * @return the response entity
-     */
     @PatchMapping("/{id}")
     public ResponseEntity<String> updateGiftCertificate(@PathVariable String id,
                                                         @RequestBody GiftCertificate giftCertificate) {
