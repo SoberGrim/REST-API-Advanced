f4bbe50f7c8c99c5d4ac686daf9d12aa54d742c9
Peachex <klevolex@gmail.com>
2021-06-27 16:42:33 +0300

    update build.gradle

--- repository/src/test/java/com/epam/esm/config/DataSourceConfig.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.config;
-
-import javax.sql.DataSource;
-
-public class DataSourceConfig {
-    public static final DataSource dataSource = new DataSourceConfiguration().embeddedDataSource();
-}
---
+++ repository/src/test/java/com/epam/esm/config/EntityManagerFactoryConfiguration.java
@@ -0,0 +1,37 @@
+package com.epam.esm.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@TestConfiguration
+public class DataSourceTestConfiguration {
+    private static final String CREATE_DATABASE_SCRIPT = "classpath:script/schema.sql";
+    private static final String FILL_DATABASE_WITH_DATA_SCRIPT = "classpath:script/data.sql";
+    private static final String PACKAGE_TO_SCAN = "com.epam.esm";
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(embeddedDataSource());
+        em.setPackagesToScan(PACKAGE_TO_SCAN);
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        return em;
+    }
+
+    @Bean
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(CREATE_DATABASE_SCRIPT)
+                .addScript(FILL_DATABASE_WITH_DATA_SCRIPT)
+                .build();
+    }
+}
